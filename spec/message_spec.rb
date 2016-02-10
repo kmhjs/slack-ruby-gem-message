12cb17585493f23ebcb70830ddbad026ec2c5915
Kazuki Murakami <kmhjs@users.noreply.github.com>
2016-02-11 03:21:26 +0900

    [ADD] Content of test

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,23 +1,134 @@
 require_relative '../lib/slack-ruby-gem-message.rb'
 
-RSpec.describe 'Message' do
+RSpec.describe 'MessageType' do
     it 'data specified as unknown message' do
-        data = {"reply_to"=>0, "type"=>"", "channel"=>"", "user"=>"", "text"=>"", "ts"=>""}
-        expect(true).to eq false
+        data = {
+            "reply_to" => 0,
+            "type" => "",
+            "channel" => "",
+            "user" => "",
+            "text" => "",
+            "ts" => ""
+        }
+        expect(data.message_type).to eq ResponseType::RawValues::UNKNOWN
     end
 
     it 'data specified as normal message' do
-        data = {"type"=>"", "channel"=>"", "user"=>"", "text"=>"", "ts"=>"", "team"=>""}
-        expect(true).to eq false
+        data = {
+            "type" => "",
+            "channel" => "",
+            "user" => "",
+            "text" => "",
+            "ts" => "",
+            "team" => ""
+        }
+        expect(data.message_type).to eq ResponseType::RawValues::NORMAL
     end
 
-    it 'data specified as reaction message' do
-        data = {"type"=>"", "deleted_ts"=>"", "subtype"=>"", "hidden"=>true, "channel"=>"", "previous_message"=>{"type"=>"", "user"=>"", "text"=>"", "ts"=>""}, "event_ts"=>"", "ts"=>""}
-        expect(true).to eq false
+    it 'data specified as deleted message' do
+        data = {
+            "type" => "",
+            "deleted_ts" => "",
+            "subtype" => "",
+            "hidden"=>true,
+            "channel" => "",
+            "previous_message" => {
+                "type" => "",
+                "user" => "",
+                "text" => "",
+                "ts" => ""
+            },
+            "event_ts" => "",
+            "ts" => ""
+        }
+        expect(data.message_type).to eq ResponseType::RawValues::DELETED
     end
 
     it 'data specified as bot message' do
-        data = {"text"=>"", "username"=>"", "icons"=>{"emoji"=>""}, "type"=>"", "subtype"=>"", "channel"=>"", "ts"=>""}
-        expect(true).to eq false
+        data = {
+            "text" => "",
+            "username" => "",
+            "icons"=> {
+                "emoji" => ""
+            },
+            "type" => "",
+            "subtype" => "",
+            "channel" => "",
+            "ts" => ""
+        }
+        expect(data.message_type).to eq ResponseType::RawValues::BOT
+    end
+end
+
+RSpec.describe 'MessageMapping' do
+    it 'data specified as unknown message' do
+        data = {
+            "reply_to" => 0,
+            "type" => "",
+            "channel" => "",
+            "user" => "",
+            "text" => "",
+            "ts" => ""
+        }
+        model = data.to_model
+
+        comp = data.inject(true) { |res, (k, v)| res &&= (v == model["#{k}"]) }
+        expect(comp).to eq true
+    end
+
+    it 'data specified as normal message' do
+        data = { 
+            "type" => "",
+            "channel" => "",
+            "user" => "",
+            "text" => "",
+            "ts" => "",
+            "team" => ""
+        }
+        model = data.to_model
+
+        comp = data.inject(true) { |res, (k, v)| res &&= (v == model["#{k}"]) }
+        expect(comp).to eq true
+    end
+
+    it 'data specified as reaction message' do
+        data = {
+            "type" => "",
+            "deleted_ts" => "",
+            "subtype" => "",
+            "hidden"=>true,
+            "channel" => "",
+            "previous_message"=>{
+                "type" => "",
+                "user" => "",
+                "text" => "",
+                "ts" => ""
+
+            },
+            "event_ts" => "",
+            "ts" => ""
+        }
+        model = data.to_model
+
+        comp = data.inject(true) { |res, (k, v)| res &&= (v == model["#{k}"]) }
+        expect(comp).to eq true
+    end
+
+    it 'data specified as bot message' do
+        data = { 
+            "text" => "",
+            "username" => "",
+            "icons"=>{ 
+                "emoji" => ""
+            },
+            "type" => "",
+            "subtype" => "",
+            "channel" => "",
+            "ts" => ""
+        }
+        model = data.to_model
+
+        comp = data.inject(true) { |res, (k, v)| res &&= (v == model["#{k}"]) }
+        expect(comp).to eq true
     end
 end
