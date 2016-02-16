080fc3b309370aa00ec7662fa95a46fca323ae6e
Kazuki Murakami <kmhjs@users.noreply.github.com>
2016-02-17 01:17:14 +0900

    [ADD] Comments

--- lib/slack-ruby-gem-message.rb
+++ lib/slack-ruby-gem-message.rb
@@ -1,31 +1,55 @@
 require 'json'
 
+#
+# A module for define type, generate struct, and mapping.
+#
 module Response
+    #
+    # A module for type definition and validation.
+    # Definitions are defined in JSON file.
+    #
     module Type
+        # Definition of models
         MODEL_DEFINITION = open('../config/types.json') { |io|
             JSON.load(io)
         }
 
+        # Returns defined types list.
         def self.types
             MODEL_DEFINITION.keys
         end
 
+        # Returns sorted fields for type.
+        # If not defined type was given, empty list will be returned.
         def self.fields_for(type)
             MODEL_DEFINITION[type].to_a.sort
         end
 
+        # Validates the type is defined or not.
         def self.defined?(type)
             self.types.include?(type)
         end
     end
 
+    #
+    # A module for generating Struct class for given fields.
+    # Members of Struct class are given fields and :struct_type.
+    # :struct_type will contain the type name of Struct.
+    #
     module StructGenerator
+        # Returns Struct class which have members defined in fields and
+        # :struct_type.
         def self.generate(fields)
             eval('Struct.new(:struct_type, "' + fields.join('", "') + '")')
         end
     end
 
+    #
+    # A module for mapping to type and struct from hash.
+    #
     module Mapper
+        # Returns type of given hash.
+        # If type of hash is not defined, 'Unknown' will be returned.
         def self.to_type(hash)
             fields = hash.keys.sort
 
@@ -37,6 +61,7 @@ module Response
             type.nil? ? "Unknown" : type
         end
 
+        # Convert hash into Struct class instance.
         def self.to_struct(hash)
             type = self.to_type(hash)
             fields = Type.fields_for(type)
@@ -54,15 +79,21 @@ module Response
     end
 end
 
+#
+# A class extension of Hash class for Slack response object.
+#
 class Hash
+    # Validates the hash is defined type or not.
     def is_type_of(type)
         self.keys.sort == Response::Type.fields_for(type)
     end
 
+    # Returns type for hash structure.
     def type
         Response::Mapper.to_type(self)
     end
 
+    # Converts into struct mapped object.
     def to_struct
         Response::Mapper.to_struct(self)
     end
